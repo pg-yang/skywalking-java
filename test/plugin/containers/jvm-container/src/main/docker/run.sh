6b3bacc6e9e2e0fcb2e81b3818068a260c46536e
pg.yang <pg.yang@hotmail.com>
2022-08-17 13:05:37 +0800

    fff

--- test/plugin/containers/jvm-container/src/main/docker/run.sh
+++ test/plugin/containers/jvm-container/src/main/docker/run.sh
@@ -84,7 +84,7 @@ export agent_opts="
     -Dskywalking.meter.report_interval=1
     -Xms256m -Xmx256m ${agent_opts}"
 
-bash /var/run/${SCENARIO_NAME}/${SCENARIO_START_SCRIPT} 1>${LOGS_HOME}/scenario.out &
+bash /var/run/${SCENARIO_NAME}/${SCENARIO_START_SCRIPT} 1>${LOGS_HOME}/scenario.out 2>${LOGS_HOME}/scenario-error.out &
 
 healthCheck ${SCENARIO_HEALTH_CHECK_URL}
 
@@ -98,6 +98,7 @@ curl -s --max-time 3 http://localhost:12800/receiveData > ${SCENARIO_HOME}/data/
 
 echo "logs -> "
 cat ${LOGS_HOME}/scenario.out
+cat ${LOGS_HOME}/scenario-error.out
 
 echo "To validate"
 java -jar \
