c1ece248fe5ba2a48ea46387d75bb44f1347a5a9
pg.yang <pg.yang@hotmail.com>
2022-08-28 20:36:34 +0800

    temple save

---
+++ test/plugin/scenarios/jedis-3.1.x-scenario/src/main/java/org/apache/skywalking/apm/testcase/jedis/controller/RedisStreamCommandExecutor.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.skywalking.apm.testcase.jedis.controller;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.StreamEntryID;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RedisStreamCommandExecutor implements AutoCloseable {
+    private Jedis jedis;
+
+    public RedisStreamCommandExecutor(String host, Integer port) {
+        jedis = new Jedis(host, port);
+    }
+
+    public void set(String key, String value) {
+
+        HashMap<String, String> hash = new HashMap<>();
+        hash.put("a", "1");
+        jedis.xadd("abc", new StreamEntryID("0-1"), hash);
+
+
+        Map<String, StreamEntryID> hashMap = new HashMap<>();
+        hashMap.put("abc", new StreamEntryID("0-1"));
+        // adapt  jedis 3.6.0 jedis.xread(new XReadParams().count(1), hashMap);
+        // and jedis.3.5.2  xread(final int count, final long block, final Entry<String, StreamEntryID>... streams)
+        JedisXreadparamAdaptor.callXReadMethod(jedis, 1, hashMap);
+    }
+
+    public void close() throws Exception {
+        jedis.close();
+    }
+}
